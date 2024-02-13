75a9f7866b2a0d130686481950001905d62e385c
utility-libraries-star <114432586+utility-libraries-star@users.noreply.github.com>
2024-02-13 17:34:21 +0300

    Update install-widget-on-godaddy.js

--- go-daddy/install-widget-on-godaddy.js
+++ go-daddy/install-widget-on-godaddy.js
@@ -1,1 +1,91 @@
-class InstallWidget{constructor(t){this.isAddedWidget=!1,this.platformUrl=t,this.currentFrame=window.frameElement,this.parentElement=this.currentFrame&&this.currentFrame.parentNode,this.body=this.parentElement&&this.parentElement.ownerDocument.body}installWidget(t,e){let i=document.createElement("div");if(i.classList.add(t),this.isAddedWidget=!0,e){this.body.prepend(i);return}this.parentElement.appendChild(i)}installPlatform(){let t=document.createElement("script");t.src=this.platformUrl,t.dataset.useServiceCore="true",this.parentElement.appendChild(t)}installStyles(){let t=document.createElement("style");t.textContent=".widget_section, .widget_section :is([data-ux=Grid],[data-ux=GridCell],[data-ux=Element],iframe) {margin: 0!important; padding: 0!important;}",this.body.appendChild(t)}getAttribute(t,e){if(t&&e){let i=t.getAttribute(e);return""===i||i}return null}getWidgetBlock(t,e){return t.querySelector('[class*="'+(e||"elfsight-app")+'"]')}removeOldCode(t){let e=this.parentElement.querySelectorAll(t);e.length&&e.forEach(function(t){t.remove()})}initial(){if(!this.currentFrame)return;let t=!this.currentFrame.baseURI.includes("editor"),e=this.getWidgetBlock(document);if(e&&t&&!this.isAddedWidget){let i=this.getAttribute(e,"class"),r=this.getAttribute(e,"floating");this.parentElement.closest("section").classList.add("widget_section"),this.installStyles(),this.getWidgetBlock(this.parentElement,i)?this.removeOldCode('iframe[srcdoc*="'+i+'"]'):requestAnimationFrame(()=>{this.installPlatform(),this.installWidget(i,r),this.currentFrame.remove()})}}}document.addEventListener("DOMContentLoaded",()=>{let t=new InstallWidget("https://static.elfsight.com/platform/platform.js");t.initial()},!0);
+class InstallWidget {
+  constructor(url) {
+    this.isAddedWidget = false;
+    this.platformUrl = url;
+
+    this.currentFrame = window.frameElement;
+    this.parentElement = this.currentFrame && this.currentFrame.parentNode;
+    this.body = this.parentElement && this.parentElement.ownerDocument.body;
+  }
+
+  installWidget(className, isFloating) {
+    const container = document.createElement('div');
+    container.classList.add(className);
+    this.isAddedWidget = true;
+
+    if (isFloating) {
+      this.body.prepend(container);
+      return;
+    }
+    this.parentElement.appendChild(container);
+  }
+
+  installPlatform() {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = this.platformUrl;
+    scriptElement.dataset.useServiceCore = 'true';
+    this.parentElement.appendChild(scriptElement);
+  }
+
+  installStyles() {
+    const styleElement = document.createElement('style');
+    styleElement.textContent =
+      '.widget_section, .widget_section :is([data-ux=Grid],[data-ux=GridCell],[data-ux=Element],iframe) {margin: 0!important; padding: 0!important;}';
+    this.body.appendChild(styleElement);
+  }
+
+  getAttribute(element, attributeName) {
+    if (element && attributeName) {
+      const attributeValue = element.getAttribute(attributeName);
+      return attributeValue === '' || attributeValue;
+    }
+    return null;
+  }
+
+  getWidgetBlock(parentElement, className) {
+    const currentClassName = className || 'elfsight-app';
+    return parentElement.querySelector('[class*="' + currentClassName + '"]');
+  }
+
+  removeOldCode(selector) {
+    const nodes = this.parentElement.querySelectorAll(selector);
+    if (nodes.length) {
+      nodes.forEach(function (block) {
+        block.remove();
+      });
+    }
+  }
+
+  initial() {
+    if (!this.currentFrame) return;
+
+    const isNotEditor = !this.currentFrame.baseURI.includes('editor');
+    const widgetElement = this.getWidgetBlock(document);
+
+    if (widgetElement && isNotEditor && !this.isAddedWidget) {
+      const className = this.getAttribute(widgetElement, 'class');
+      const isFloating = this.getAttribute(widgetElement, 'floating');
+
+      this.parentElement.closest('section').classList.add('widget_section');
+      this.installStyles();
+
+      console.log(this.getWidgetBlock(this.parentElement, className));
+      if (this.getWidgetBlock(this.parentElement, className)) {
+        this.removeOldCode('iframe[srcdoc*="' + className + '"]');
+      } else {
+        requestAnimationFrame(() => {
+          this.installPlatform();
+          this.installWidget(className, isFloating);
+          this.currentFrame.remove();
+        });
+      }
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const installWidget = new InstallWidget(
+    'https://static.elfsight.com/platform/platform.js'
+  );
+  installWidget.initial();
+});
