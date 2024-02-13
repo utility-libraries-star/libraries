307ffe9129e5c86033e82bc87e286f428a519830
utility-libraries-star <114432586+utility-libraries-star@users.noreply.github.com>
2024-02-13 17:24:25 +0300

    Update install-widget-on-godaddy.js

--- go-daddy/install-widget-on-godaddy.js
+++ go-daddy/install-widget-on-godaddy.js
@@ -1,1 +1,88 @@
-var WIDGET_BASE_URL="https://static.elfsight.com/platform/platform.js",insertWidget=function t(e,i,r){var n=document.createElement("div");return n.classList.add(i),r?e.ownerDocument.body.prepend(n):e.appendChild(n)},loadPlatformScript=function t(e){var i=document.createElement("script");i.src=WIDGET_BASE_URL,i.dataset.useServiceCore="true",e.appendChild(i)},insertStyles=function t(e){var i=document.createElement("style");i.textContent=".section_with_elfsigth_widget, .section_with_elfsigth_widget :is([data-ux=Grid],[data-ux=GridCell],[data-ux=Element],iframe) {margin: 0!important; padding: 0!important;}",e.prepend(i)},getAttributeValue=function t(e,i){if(e&&i){var r=e.getAttribute(i);return""===r||r}return null},getWidgetBlock=function t(e){var i=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"elfsight-app";return e.querySelector('[class*="'.concat(i,'"]'))},initializeWidgetSetup=function t(){var e=window.frameElement;if(e){var i=!e.baseURI.includes("editor"),r=getWidgetBlock(document);if(r&&i){var n=getAttributeValue(r,"class"),a=getAttributeValue(r,"floating"),l=e.parentNode;l.closest("section").classList.add("section_with_elfsigth_widget"),insertStyles(l),getWidgetBlock(l,n)?l.querySelectorAll('iframe[srcdoc*="'.concat(n,'"]')).forEach(function(t){return t.remove()}):(loadPlatformScript(l),insertWidget(l,n,a),e.remove())}}};document.addEventListener("DOMContentLoaded",initializeWidgetSetup);
+class InstallWidget {
+  constructor(url) {
+    this.isAddedWidget = false;
+    this.platformUrl = url;
+
+    this.currentFrame = window.frameElement;
+    this.parentElement = this.currentFrame && this.currentFrame.parentNode;
+    this.body = this.parentElement && this.parentElement.ownerDocument.body;
+  }
+
+  installWidget(className, isFloating) {
+    const container = document.createElement('div');
+    container.classList.add(className);
+    this.isAddedWidget = true;
+
+    if (isFloating) {
+      this.body.prepend(container);
+      return;
+    }
+    this.parentElement.appendChild(container);
+  }
+
+  installPlatform() {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = this.platformUrl;
+    scriptElement.dataset.useServiceCore = 'true';
+    this.parentElement.appendChild(scriptElement);
+  }
+
+  installStyles() {
+    const styleElement = document.createElement('style');
+    styleElement.textContent =
+      '.widget_section, .widget_section :is([data-ux=Grid],[data-ux=GridCell],[data-ux=Element],iframe) {margin: 0!important; padding: 0!important;}';
+    this.body.appendChild(styleElement);
+  }
+
+  getAttribute(element, attributeName) {
+    if (element && attributeName) {
+      const attributeValue = element.getAttribute(attributeName);
+      return attributeValue === '' || attributeValue;
+    }
+    return null;
+  }
+
+  getWidgetBlock(parentElement, className) {
+    const currentClassName = className || 'elfsight-app';
+    return parentElement.querySelector('[class*="' + currentClassName + '"]');
+  }
+
+  removeOldCode(selector) {
+    const nodes = this.parentElement.querySelectorAll(selector);
+    if (nodes.length) {
+      nodes.forEach(function (block) {
+        block.remove();
+      });
+    }
+  }
+
+  initial() {
+    if (!this.currentFrame) return;
+
+    const isNotEditor = !this.currentFrame.baseURI.includes('editor');
+    const widgetElement = this.getWidgetBlock(document);
+
+    if (widgetElement && isNotEditor && !this.isAddedWidget) {
+      const className = this.getAttribute(widgetElement, 'class');
+      const isFloating = this.getAttribute(widgetElement, 'floating');
+
+      this.parentElement.closest('section').classList.add('widget_section');
+      this.installStyles();
+
+      if (this.getWidgetBlock(this.parentElement, className)) {
+        this.removeOldCode('iframe[srcdoc*="' + className + '"]');
+      } else {
+        this.installPlatform();
+        this.installWidget(className, isFloating);
+        this.currentFrame.remove();
+      }
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const installWidget = new InstallWidget(
+    'https://static.elfsight.com/platform/platform.js'
+  );
+  installWidget.initial();
+});
